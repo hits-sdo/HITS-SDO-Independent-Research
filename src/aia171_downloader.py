5d1b295ddf30324ccb0811c6ea06a22decc5b048
Nicholas King <starfractor@gmail.com>
2023-07-03 14:54:48 -0700

    Able to download color miniset

--- src/aia171_downloader.py
+++ src/aia171_downloader.py
@@ -3,9 +3,20 @@ import gdown
 import tarfile
 
 def main():
-    # Download file
+    # Download file BW Miniset
     url = 'https://drive.google.com/u/4/uc?id=16WD0td1f5gx4yIIDkWWSTb-oZcezI1CU&export=download'
     output = './AIA171_Miniset_BW.tar.gz'
+    gdown.download(url, output, quiet=False)
+
+    # Get tar file
+    tar = tarfile.open(output, 'r:gz')
+    
+    # extracting file
+    tar.extractall()
+
+    # Download color Miniset
+    url = 'https://drive.google.com/u/4/uc?id=15C5spf1la7L09kvWXll2qt67Ec0rwLsY&export=download'
+    output = './aia_171_color_1perMonth.tar.gz'
     gdown.download(url, output, quiet=False)
 
     # Get tar file
